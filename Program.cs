60d024088bb80514900086265932ec744cb695cd
Vinicius Costa <oviniciusc@gmail.com>
2022-02-24 00:11:23 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace GeradorDeSenhas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtd = int.Parse(args[0]);
+            string caracteresPossiveis = "ABCDEFGHIJKLMNOPQRSTUVZXYZÇ" + "ABCDEFGHIJKLMNOPQRSTUVZXYZÇ".ToLower() +
+            "0123456789" + "!@#$%";
+
+            Random sorteio = new Random();
+            int numeroSorteio = 0;
+            StringBuilder password = new StringBuilder();
+
+            for (int t = 0; t < qtd; t++)
+            {
+                numeroSorteio = sorteio.Next(0, caracteresPossiveis.Length - 1);
+                password.Append(caracteresPossiveis[numeroSorteio]);
+            }
+            Console.WriteLine();
+            Console.Write(password);
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
